976bc1e968f50edeb69578e5f734b5300bc0fc6c
Daniel Eldjarn <danielv@dohop.com>
2022-04-03 02:09:18 +0000

    Add zsh config files

---
+++ .env.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# Specify default editor. Possible values: vim, nano, ed etc.
+export EDITOR=vim
+
+# Add brew
+export PATH="/opt/homebrew/bin:$PATH"
+
+# Add programs installed by git
+export PATH="/opt/homebrew/bin:$PATH"
